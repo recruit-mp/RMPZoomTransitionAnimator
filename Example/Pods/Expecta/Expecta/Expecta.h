7c1d65507f358384622392b029790b232c7ae39b
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2015-03-26 14:50:03 +0900

    Setup by cocoapods template

---
+++ Example/Pods/Expecta/Expecta/Expecta.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Expecta.
+FOUNDATION_EXPORT double ExpectaVersionNumber;
+
+//! Project version string for Expecta.
+FOUNDATION_EXPORT const unsigned char ExpectaVersionString[];
+
+#import <Expecta/ExpectaObject.h>
+#import <Expecta/ExpectaSupport.h>
+#import <Expecta/EXPMatchers.h>
+
+// Enable shorthand by default
+#define expect(...) EXP_expect((__VA_ARGS__))
---
+++ Example/Pods/Expecta/Expecta/Matchers/EXPMatchers+postNotification.h
@@ -0,0 +1,5 @@
+#import "Expecta.h"
+
+EXPMatcherInterface(postNotification, (id expectedNotification));
+
+#define notify postNotification
